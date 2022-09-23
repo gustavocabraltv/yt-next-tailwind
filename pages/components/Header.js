950b031a39e16cea1e84f41650af05bc5fbf7851
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-09-23 18:11:32 -0300

    Header mobile fixed

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className=' sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md w-screen justify-center '>
-        <div className='flex h-24 max-w-[1140px] justify-between w-full items-center px-8'>
+        <div className='flex h-24 max-w-[1140px] justify-between w-full items-center px-8 md:px-0 lg:px-0'>
             <div>
             <h1>UI Expert</h1>
             </div>
