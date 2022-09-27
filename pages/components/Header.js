e48708d2a3e8dd6e0e9e4cc37ea8fe4b55b4a4ac
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-09-27 18:49:22 -0300

    comments here

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <div className=' sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md w-screen justify-center '>
         <div className='flex h-24 max-w-[1140px] justify-between w-full items-center px-8 md:px-4 lg:px-0'>
             <div>
-            <h1>UI Expert</h1>
+            <h1>UI Expert v2</h1>
             </div>
             <div>
             <h1>Item1</h1>
