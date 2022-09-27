893d3b7592eab281dc26c071c1551a032f3a116e
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-09-27 19:35:02 -0300

    testing

--- components/Hero.js
+++ components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
          <div className='flex max-w-[1140px] justify-between w-full items-center px-8 md:px-4 lg:px-0'>
         <div className='flex flex-col gap-4'>
             <h1 className=' text-7xl font-semibold leading-tight'>Find your new style with <span className='text-[#3865F3]'>Veshion</span></h1>
-            <p className='w-[500px] text-[#94959B] text-[20px]'>Buy something has never been easy now, let's download Veshion now and get 30% off</p>
+            <p className='w-[500px] text-[#94959B] text-[20px]'>Buy something has never been easy now, download Veshion now and get 30 off</p>
             <div className='flex flex-row gap-4'>
             <Image
                className='cursor-pointer'
--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,54 @@ export default function Home() {
       </Head>
       <Header/>
       <Hero/>
+      <Container>
+        <h1 className='text-4xl font-bold pb-[24px]'>New Features</h1>
+        <p className='text-xl text-[#94959B] pb-[64px] text-center'>We have release some new features in the Veshion. Check it out now guys</p>
+
+        <div className='flex flex-col gap-10 lg:flex-row justify-between w-full'>
+
+          <div className='flex flex-col items-center justify-center '>
+            
+            <Image
+               className='cursor-pointer'
+        src="/assets/icone-1.svg"
+        alt="Logo"
+        height={140}
+        width={140}
+      />
+            <h2 className='pb-[16px] text-2xl font-semibold pt-[64px]'>Free Delivery</h2>
+            <p>Free delivery up to 7 to all addresses</p>
+          </div>
+
+          <div className='flex flex-col items-center justify-center '>
+          <Image
+               className='cursor-pointer'
+        src="/assets/icone-1.svg"
+        alt="Logo"
+        height={140}
+        width={140}
+      />            <h2 className='pb-[16px] text-2xl font-semibold pt-[64px]'>Free Delivery</h2>
+            <p>Free delivery up to 7 to all addresses</p>
+          </div>
+
+          <div className='flex flex-col items-center justify-center '>
+          <Image
+               className='cursor-pointer'
+        src="/assets/icone-1.svg"
+        alt="Logo"
+        height={140}
+        width={140}
+      />            <h2 className='pb-[16px] text-2xl font-semibold pt-[64px]'>Free Delivery</h2>
+            <p>Free delivery up to 7 to all addresses</p>
+          </div>
+
+
+
+        </div>
+      </Container>
+    
+  
+
 
 
 
